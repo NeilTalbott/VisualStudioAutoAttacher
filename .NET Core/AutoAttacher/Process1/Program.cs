a1bc0c61d2adfd7083b1fdbfbc359ec453e83c84
Neil Talbott <neil.talbott@htilabs.com>
2022-05-04 13:36:27 +0100

    Finish class move

--- .NET Core/AutoAttacher/Process1/Program.cs
+++ .NET Core/AutoAttacher/Process1/Program.cs
@@ -4,9 +4,6 @@
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
-    using System.Runtime.InteropServices;
-    using System.Runtime.Versioning;
-    using System.Security;
     using EnvDTE80;
 
     class Program
@@ -75,56 +72,6 @@
             {
                 proc.Attach();
             }
-        }
-
-        // This method has been copied from Microsoft's source code here: https://github.com/microsoft/referencesource/blob/5697c29004a34d80acdaf5742d7e699022c64ecd/mscorlib/system/runtime/interopservices/marshal.cs#L2404
-        public static class Marshal2
-        {
-            internal const String OLEAUT32 = "oleaut32.dll";
-            internal const String OLE32 = "ole32.dll";
-
-            [System.Security.SecurityCritical]  // auto-generated_required
-            public static Object GetActiveObject(String progID)
-            {
-                Object obj = null;
-                Guid clsid;
-
-                // Call CLSIDFromProgIDEx first then fall back on CLSIDFromProgID if
-                // CLSIDFromProgIDEx doesn't exist.
-                try
-                {
-                    CLSIDFromProgIDEx(progID, out clsid);
-                }
-                //            catch
-                catch (Exception)
-                {
-                    CLSIDFromProgID(progID, out clsid);
-                }
-
-                GetActiveObject(ref clsid, IntPtr.Zero, out obj);
-                return obj;
-            }
-
-            //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
-            [DllImport(OLE32, PreserveSig = false)]
-            [ResourceExposure(ResourceScope.None)]
-            [SuppressUnmanagedCodeSecurity]
-            [System.Security.SecurityCritical]  // auto-generated
-            private static extern void CLSIDFromProgIDEx([MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid);
-
-            //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
-            [DllImport(OLE32, PreserveSig = false)]
-            [ResourceExposure(ResourceScope.None)]
-            [SuppressUnmanagedCodeSecurity]
-            [System.Security.SecurityCritical]  // auto-generated
-            private static extern void CLSIDFromProgID([MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid);
-
-            //[DllImport(Microsoft.Win32.Win32Native.OLEAUT32, PreserveSig = false)]
-            [DllImport(OLEAUT32, PreserveSig = false)]
-            [ResourceExposure(ResourceScope.None)]
-            [SuppressUnmanagedCodeSecurity]
-            [System.Security.SecurityCritical]  // auto-generated
-            private static extern void GetActiveObject(ref Guid rclsid, IntPtr reserved, [MarshalAs(UnmanagedType.Interface)] out Object ppunk);
         }
     }
 }
