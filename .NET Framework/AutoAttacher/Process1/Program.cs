dd878dae302285b58a318bf5f31291ce428ef337
Neil Talbott <neil.talbott@htilabs.com>
2022-05-04 12:50:52 +0100

    Identify and verify location of Process2 executable

--- .NET Framework/AutoAttacher/Process1/Program.cs
+++ .NET Framework/AutoAttacher/Process1/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Process1
 {
+    using System.IO;
+    using System.Reflection;
+
     class Program
     {
+        private const string Process2Name = "Process2";
+
         static void Main(string[] args)
         {
+            var currentFolder = AppDomain.CurrentDomain.RelativeSearchPath ?? AppDomain.CurrentDomain.BaseDirectory;
+            var solutionFolder = new DirectoryInfo(currentFolder).Parent.Parent.Parent;
+            var process2Folder = Path.Combine(solutionFolder.FullName, Process2Name, "bin", "Debug");
+
+            var process2Executable = Path.Combine(process2Folder, $"{Process2Name}.exe");
+
+            if (!File.Exists(process2Executable))
+            {
+                throw new ApplicationException("Executable file for Process 2 not found");
+            }
         }
     }
 }
